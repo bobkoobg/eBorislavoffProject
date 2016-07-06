877486bb1e527f91da6b32db54e06308855f839a
bobkoobg <boyko.surlev@gmail.com>
2016-07-06 18:14:54 +0200

    Implement : Styled index nav menu

--- src/scripts/backend/index.js
+++ src/scripts/backend/index.js
@@ -1,5 +1,34 @@
+var pages = [
+    {'name': 'Articles', 'href': '/emkobarona/articles.html', 'color': '#ee4035'},
+    {'name': 'Gallery', 'href': '/emkobarona/gallery.html', 'color': '#f37736'},
+    {'name': 'Tickets', 'href': '/emkobarona/ticket.html', 'color': '#fdf498'},
+    {'name': 'Guestbook', 'href': '/emkobarona/guestbook.html', 'color': '#7bc043'},
+    {'name': 'Flexible Sections', 'href': '/emkobarona/flexibleSections.html', 'color': '#0392cf'},
+    {'name': 'Users', 'href': '/emkobarona/users.html', 'color': '#000000'}
+];
+var $indexNav;
+
+function loadIndexNav() {
+    for (i = 0; i < pages.length; i++) {
+        console.log("> ", pages[i]);
+        var source = $("#index-nav-element-template").html();
+        var template = Handlebars.compile(source);
+        var content = {
+            name: pages[i].name,
+            color: pages[i].color,
+            href: pages[i].href
+        };
+        var html = template(content);
+        $indexNav.append(html);
+    }
+}
+
 function load() {
     console.log("index.js loaded...");
+
+    $indexNav = $("#index-nav");
+
+    loadIndexNav();
 }
 
 $(window).ready(load);
